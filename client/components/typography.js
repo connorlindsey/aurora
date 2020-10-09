13df95934d2212fe434537cce4b84abeccc8374e
= <=>
2020-10-08 22:47:57 -0600

    +

---
+++ client/components/typography.js
@@ -0,0 +1,48 @@
+import styled from "styled-components"
+
+const Text = styled.p`
+  text-align: ${(props) => props.textAlign};
+  margin: ${(props) => props.margin};
+  margin-bottom: ${(props) => props.mb};
+  max-width: ${(props) => props.maxWidth};
+`
+
+const Headline = styled(Text)`
+  font-size: 36px;
+  font-size: max(34px, min(42px, 3vw));
+  font-size: clamp(36px, 3vw, 48px);
+  font-weight: 400;
+  letter-spacing: 0.05rem;
+`
+
+const Subheading = styled(Text)`
+  font-size: 28px;
+  font-size: max(28px, min(36px, 3vw));
+  font-size: clamp(28px, 3vw, 36px);
+  font-weight: 600;
+`
+
+const Title = styled(Text)`
+  font-size: 19px;
+  font-size: max(19px, min(22, 3vw));
+  font-size: clamp(19px, 3vw, 22px);
+  font-weight: 400;
+`
+
+const Body = styled(Text)`
+  font-size: 17px;
+  font-size: max(17px, min(20px, 2.4vw));
+  font-size: clamp(17px, 2.4vw, 20px);
+  font-weight: 500;
+  color: ${(props) => props.theme.grey["800"]};
+`
+
+const Caption = styled(Text)`
+  font-size: 14px;
+  font-size: max(12px, min(14px, 3vw));
+  font-size: clamp(12px, 3vw, 14px);
+  font-weight: 500;
+  color: ${(props) => props.theme.grey["800"]};
+`
+
+export { Headline, Subheading, Title, Body, Caption }
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,28 +1,101 @@
+
 import Head from 'next/head'
 import Link from "next/link"
+import { useEffect, useState} from 'react'
 import styled from "styled-components";
 
+const nouns = ['aims', 'tasks', 'initiatives', 'goals', 'habits', 'time', 'challenges', 'targets']
+
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>twelvemonth</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	const [keyword, setKeyword]	= useState("aims")
+	
+	useEffect(() => { 
+		const interval = setInterval(() => { 
+			let num = Math.floor(Math.random() * nouns.length) 
+			setKeyword(nouns[num])}, 2500) 
 
-      <main>
-        <Headline>twelvemonth</Headline>
-        <p>Track your aims simply and consistently.</p>
-      </main>
+		return () => clearInterval(interval)
+	}, [])
 
-      <nav>
-        <Link href="/dashboard"><a>Dashboard</a></Link>
-      </nav>
-    </div>
+	return (
+		<Layout>
+			<Main>
+				<Headline>12month</Headline>
+				<Tagline>A tool for tracking life's most important <KeyWord>{keyword}</KeyWord>.</Tagline>
+				<SignupContainer>
+					<h2>Early Access</h2>	
+					<p style={{textAlign: 'justify'}}>Our app is currently in development and we could really use your help. Sign up below and we'll reach out to your with <u>updates</u>, <u>questions</u>, and <u>requests for feedback.</u></p>	
+					<Input placeholder="name"></Input>
+					<Input placeholder="email"></Input>
+					<Submit type="submit">Submit</Submit>
+				</SignupContainer>    
+			</Main>
+		</Layout>
   )
 }
 
+const Submit = styled.button`
+	background-color: #5CD670;
+	padding: .5rem 1rem;
+	border-radius: 3px;
+	border: none;
+	margin: 1rem;
+		
+	&:hover {
+		background-color:#2D3139;
+		border: solid 1px #5CD670; 
+		color: #5CD670;
+	}
+
+`
+const Input = styled.input`
+	background: transparent;
+	border: solid .5px white;
+	border-radius: 3px;
+	color: white;
+	padding: .5rem;
+	margin: 1.25rem .5rem;
+`
 const Headline = styled.h1`
-  font-size: 2rem;
-  color: #1290be;
+	font-size: 2rem;
+	color: white;
+`
+const Tagline = styled.h2`
+	text-align: center;
+	margin: .5rem 5rem;
+`
+
+const KeyWord = styled.span`
+	color: #5CD670;
+	text-decoration: underline;
+`
+const Layout = styled.div`
+	display: flex;
+	justify-content: center;
+    align-items: center;	
+	background-color: #1E2128;
+	height: 100vh;
+	width: 100vw;
+	overflow-x:hidden;
+	color: white;
+`
+const Main = styled.main`
+	display:flex;
+	align-items:center;
+	flex-direction: column;
+`
+const SignupContainer = styled.div`
+	width: 50%;
+	min-height: 400px;
+	border-radius: 3px;
+	box-shadow: 0 3px 6px -2px #000;
+  	
+	background-color: #2D3139;
+	text-align: center;
+	margin: 2rem;
+	padding: 4rem;
+	 
+	@media (max-width: 768px) {
+		width: 80%;
+	}
 `
